b7c64b7e06845c39f14b01287d94a6fb448c8390
Michael Rivnak <mrivnak86@gmail.com>
2020-05-16 17:59:40 -0400

    Q1 and Q2 done

--- Programming Languages/F#/Lab 1/Q1.fs
+++ Programming Languages/F#/Lab 1/Q1.fs
@@ -1,8 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+// Implement an add function, run the following tests
+//      add 25 15
+//      add 18 34 13 19
+
 open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let add in0 in1 =
+        in0 + in1
+
+    let rec sum list =
+        match list with
+        | [] -> 0
+        | x :: xs -> add x (sum xs)
+
+    printfn "%d" (add 25 15)
+    printfn "%d" (sum [18; 34; 13; 19])
     0 // return an integer exit code
--- Programming Languages/F#/Lab 1/Q2.fs
+++ Programming Languages/F#/Lab 1/Q2.fs
@@ -1,8 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+// Write a program that uses loop.
+// Compute the average of a list of integers.
+// Use List comprehension to generate the List.
+
 open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let add in0 in1 =
+        in0 + in1
+
+    let rec sum arr =
+        match arr with
+        | [] -> 0
+        | x :: xs -> add x (sum xs)
+
+    let rec length arr =
+        match arr with
+        | [] -> 0
+        | x :: xs -> 1 + length xs
+
+    let avg arr =
+        match arr with
+        | [] -> 0.0
+        | x -> float (sum arr) / float (length arr)
+    printfn "%f" (avg [1..4])
     0 // return an integer exit code
