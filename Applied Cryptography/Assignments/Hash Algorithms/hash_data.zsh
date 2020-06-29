cba0fd50665804c85b18fc73f5ab0378663a5596
Michael Rivnak <mrivnak86@gmail.com>
2020-06-29 09:27:47 -0400

    Working script

---
+++ Applied Cryptography/Assignments/Hash Algorithms/hash_data.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/zsh
+
+echo "Cleaning previous data..."
+
+rm hash_times.txt hashes.d/*
+
+echo "\nCalculating hashes..."
+
+hash_names=('BLAKE2b512' 'MD5' 'SHA1' 'RIPEMD-160' 'SHA224' 'SHA256' 'SHA384' 'SHA512' 'SHA3-224' 'SHA3-256' 'SHA3-384' 'SHA3-512' 'SHAKE128' 'SHAKE256')
+hash_filenames=('blake2b512' 'md5' 'sha1' 'ripemd160' 'sha224' 'sha256' 'sha384' 'sha512' 'sha3-224' 'sha3-256' 'sha3-384' 'sha3-512' 'shake128' 'shake256')
+
+TIMEFMT=$'\nreal\t%*E\nuser\t%*U\nsys\t\t%*S\n'
+
+for ((i = 1; i <= ${#hash_names[@]}; i++)); do
+    echo "[$i/${#hash_names[@]}] ${hash_names[$i]}..."
+    echo "${hash_names[$i]}:" >> hash_times.txt
+    {
+        time (
+            for ((j = 0; j < 1000; j++)); do
+                openssl ${hash_filenames[$i]} data.txt >> ./hashes.d/${hash_filenames[$i]}
+            done
+        )
+    } 2>> hash_times.txt
+done
+
+echo "\nValidating hashes..."
+
+for ((i = 1; i <= ${#hash_names[@]}; i++)); do
+    echo "[$i/${#hash_names[@]}] ${hash_names[$i]}..."
+    awk '!unique[$0]++ { count++ } END { print count == 1 ? "Passed!" : "Failed! :("  }' ./hashes.d/${hash_filenames[$i]}
+done
