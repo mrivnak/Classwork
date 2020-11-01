da9da02ae7ae2f7ed1299c271229b9a40527aec9
Michael Rivnak <mrivnak86@gmail.com>
2020-11-01 01:29:21 -0400

    added start and end script tags

--- System Administration/lab2/login.sh
+++ System Administration/lab2/login.sh
@@ -1,3 +1,5 @@
+#START SCRIPT
+#rivnakm
 #!/bin/bash
 
 FULLNAME="$(grep $USERNAME /etc/passwd | cut -d ":" -f 5)"
@@ -5,3 +7,4 @@ FULLNAME="$(grep $USERNAME /etc/passwd | cut -d ":" -f 5)"
 echo "Welcome to $HOSTNAME, $FULLNAME"
 echo "You are logged in as $USERNAME and your current directory is $PWD"
 echo "The time is $(date +%l:%M%P)"
+#END SCRIPT
