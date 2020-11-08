8c51b262f3bb5ec84035cdd2fa90f7d5a75bd164
Michael Rivnak <mrivnak86@gmail.com>
2020-11-08 13:22:32 -0500

    lab3 done

--- System Administration/lab3/account_list.sh
+++ System Administration/lab3/account_list.sh
@@ -2,4 +2,17 @@
 #rivnakm
 #!/bin/bash
 
+while read line
+do
+    if [[ "$(echo $line | cut -f 3 -d ':')" -ge 1000 ]]
+    then
+        if [[ "$(echo $line | cut -f 3 -d ':')" == "/bin/bash" ]]
+        then
+            echo "$(echo $line | cut -f 1 -d ':')"
+        else
+            echo "$(echo $line | cut -f 1 -d ':')*"
+        fi
+    fi
+done < /etc/passwd
+
 #END SCRIPT
--- System Administration/lab3/get_external_ip.sh
+++ System Administration/lab3/get_external_ip.sh
@@ -2,4 +2,14 @@
 #rivnakm
 #!/bin/bash
 
+page=$(wget -qO- http://ipchicken.com)
+
+if [[ $page =~ ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) ]]
+then
+    ip=${BASH_REMATCH[1]}
+    echo $ip
+else
+    echo $page
+fi
+
 #END SCRIPT
